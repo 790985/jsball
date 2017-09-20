72ced1e6547e00b21833982228b94bdf04ff4459
790985 <790985@seq.org>
2017-09-20 13:19:40 -0700

    Update Jsvector.js

--- Jsvector.js
+++ Jsvector.js
@@ -28,9 +28,8 @@ Vector.prototype.copy = function(){
   return new JSVector(this.x,this.y);
 }
 Vector.prototype.setMag = function(){
-  var dir = this.getDirection();
-  this.x = Math.cos(dir) * magnitude;
-  this.y = Math.sin(dir) * magnitude;
+  this.x = Math.cos(this.getDir) * this.getMag();
+  this.y = Math.sin(this.getDir) * this.getMag();
 }
 Vector.prototype.getMag = function(){
   return Math.sqrt(this.x * this.x + this.y * this.y);
@@ -38,11 +37,13 @@ Vector.prototype.getMag = function(){
 Vector.prototype.getDir = function(){
   return Math.atan2(this.y,this.x);
 }
-Vector.prototype.setDir = function(){
-  
+Vector.prototype.setDir = function(angle){
+  this.x = this.getMag * Math.cos(angle);
+  this.y = this.getMag * Math.sin(angle);
 }
 Vector.prototype.normalize = function(){
-  this.divide(new JSVector(this.getMag(),this.getMag()))
+  
+  //this.divide(new JSVector(this.getMag(),this.getMag()))
 }
 Vector.prototype.lerp = function(){
   
@@ -55,6 +56,7 @@ Vector.prototype.limit = function(factor, maximum){
     this.y *= factor;
   }
 Vector.prototype.distance(vector){}
+  this
   
 }
 Vector.prototype.angleBetween(vector){
